55afb5232c3b63f3510c3268ba69df1412b59340
cameroncuster <custer.cameron@gmail.com>
2021-04-18 00:30:43 -0600

    use NONE

--- randomPlayer.cpp
+++ randomPlayer.cpp
@@ -15,7 +15,7 @@ ValidMove RandomPlayer::makeMove( Simulatefield *pf )
 	ValidMove move = NONE;
 	vector<ValidMove> randomMoves = { UP, DOWN, LEFT, RIGHT };
 	shuffle( randomMoves.begin( ), randomMoves.end( ), default_random_engine( chrono::system_clock::now().time_since_epoch().count() ) );
-	//randomMoves.push_back( NONE );
+	randomMoves.push_back( NONE );
 	bool valid = 0;
 	for( unsigned i = 0; i < randomMoves.size( ) && !valid; i++ )
 	{
