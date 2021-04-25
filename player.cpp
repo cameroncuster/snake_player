aa80e9e9ccbd90ed3826b22f5a135d6c956df1f9
cameroncuster <custer.cameron@gmail.com>
2021-04-24 21:50:31 -0600

    udpated

--- player.cpp
+++ player.cpp
@@ -13,13 +13,44 @@ Player::Player( ) { }
 
 ValidMove Player::makeMove( const Playfield *pf )
 {
+	vector<vector<int>> grid = pf->getGrid();
+	int w = grid[0].size( );
+	int h = grid.size( );
+	queue<pair<int, int>> tail = pf->getTail();
+	pair<int, int> head = pf->headPosition();
+	pair<int, int> food = pf->foodPosition();
+	pair<int, int> tailpt = tail.front();
+	int headNode = head.first * w + head.second;
+	int foodNode = food.first * w + food.second;
+	int tailNode = tailpt.first * w + tailpt.second;
+
 	if( path.empty( ) )
 	{
 		Cycle c( pf );
 		path = c.cycle( );
+		// check if a path exists - scan otherwise
+		if( path.empty( ) )
+		{
+			if( inBounds( w, h, head.first - 1, head.second ) )
+				if( !grid[head.first - 1][head.second] )
+					return UP;
+
+			if( inBounds( w, h, head.first, head.second + 1 ) )
+				if( !grid[head.first][head.second + 1] )
+					return RIGHT;
+
+			if( inBounds( w, h, head.first + 1, head.second ) )
+				if( !grid[head.first + 1][head.second] )
+					return DOWN;
+
+			if( inBounds( w, h, head.first, head.second - 1 ) )
+				if( !grid[head.first][head.second - 1] )
+					return LEFT;
+		}
+
 	}
-    int n = path.front( );
-    path.pop_front( );
+	int n = path.front( );
+	path.pop_front( );
 	return nextMove( pf->getGrid( )[0].size( ),
 			pf->headPosition( ).first * pf->getGrid( )[0].size( ) +
 			pf->headPosition( ).second, n );
