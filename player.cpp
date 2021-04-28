f6821d5fa0a5c2ee3ee04749c1f422ecd2d6a2da
101061877 <cameron.custer@mines.sdsmt.edu>
2021-04-28 17:14:30 -0600

    updated for clean player construction

--- player.cpp
+++ player.cpp
@@ -5,9 +5,6 @@
 #include "player.h"
 #include "cycle.h"
 
-// DEBUG
-#include <cassert>
-
 using namespace std;
 
 extern ValidMove nextMove( int, int, int );
@@ -15,30 +12,24 @@ extern bool inBounds( const int, const int, const int, const int );
 
 Player::Player( ) { }
 
+// construct a cycle and follow the cycle (paths are cached)
 ValidMove Player::makeMove( const Playfield *pf )
 {
-    vector<vector<int>> grid = pf->getGrid();
-    int w = grid[0].size( );
-    int h = grid.size( );
-    pair<int, int> head = pf->headPosition();
-    pair<int, int> food = pf->foodPosition();
-    pair<int, int> tailpt = tail.empty( ) ? head : tail.front();
-    int headNode = head.first * w + head.second;
-    int foodNode = food.first * w + food.second;
-    int tailNode = tailpt.first * w + tailpt.second;
-
+    // construct a cycle if the cached path is empty
     if( path.empty( ) )
     {
         Cycle c( pf, tail );
         path = c.cycle( );
     }
-    int n = path.front( );
+    int next = path.front( );
     path.pop_front( );
 
-    tail.push( head );
-    if( grid[n / w][n % w] != FOOD_VALUE ) tail.pop( );
+    // track my own tail
+    tail.push( pf->headPosition( ) );
+    if( pf->getGrid( )[next / pf->getGrid( )[0].size( )][next % pf->getGrid( )[0].size( )] != FOOD_VALUE ) tail.pop( );
 
+    // translate the next point to a move to be executed
     return nextMove( pf->getGrid( )[0].size( ),
             pf->headPosition( ).first * pf->getGrid( )[0].size( ) +
-            pf->headPosition( ).second, n );
+            pf->headPosition( ).second, next );
 }
