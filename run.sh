1be969f3927f4a61ef52055969096b49f262d579
101061877 <cameron.custer@mines.sdsmt.edu>
2021-04-30 00:39:34 -0600

    updated for run script and local script to export LD_LIBRARY_PATH to local directory

--- run.sh
+++ run.sh
@@ -1,5 +1,27 @@
-for count in $(seq 1 100)
+# record scores
+for ((i = 0; i < 100; i++))
 do
-	./driver 20 20 >> results.dat
+    ./driver 3 50  >> results03x50.dat
+    ./driver 3 40  >> results03x40.dat
+    ./driver 5 5   >> results05x05.dat
+    ./driver 10 10 >> results10x10.dat
+    ./driver 10 20 >> results10x20.dat
+    ./driver 10 30 >> results10x30.dat
+    ./driver 20 20 >> results20x20.dat
+    ./driver 20 30 >> results20x30.dat
+    ./driver 30 30 >> results30x30.dat
 done
-./avgScores.pl < results.dat
+
+# average scores
+./avgScores.pl < results03x50.dat
+./avgScores.pl < results03x40.dat
+./avgScores.pl < results05x05.dat
+./avgScores.pl < results10x10.dat
+./avgScores.pl < results10x20.dat
+./avgScores.pl < results10x30.dat
+./avgScores.pl < results20x20.dat
+./avgScores.pl < results20x30.dat
+./avgScores.pl < results30x30.dat
+
+# clean-up
+rm *.dat
