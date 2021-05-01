88794e70d33c7056ef2332744787c589c374734a
101061877 <cameron.custer@mines.sdsmt.edu>
2021-05-01 15:10:08 -0600

    fixed run scritpts to track min correctly

--- avgScores.pl
+++ avgScores.pl
@@ -4,7 +4,7 @@
 my $sum = 0;
 my $count = 0;
 my $max = 0; 
-my $min = 0; 
+my $min = 4080;
 my $time = 0.0; 
 
 while ($line = <>)
@@ -14,8 +14,8 @@ while ($line = <>)
    {
       $sum += $1;
       $count++;
-      $max = ($1 > $max) ? $1 : $max
-      $min = ($1 < $min) ? $1 : $min
+      $max = ($1 > $max) ? $1 : $max;
+      $min = ($1 < $min) ? $1 : $min;
    }
    if ($line =~ /Average turn time:\s*(\d+\.\d+e?[-+]?\d+)/)
    {
